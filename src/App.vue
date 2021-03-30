2723886615704dc7676d3e48d6ecdc86fb647806
Ne0Brain <neobrainpower@gmail.com>
2021-03-31 07:03:36 +0800

    add Header in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
     <div class="container">
         {{msg}}
+        <app-header></app-header>
         <router-view></router-view>
     </div>
 </template>
 
 <script>
-
+import Header from './component/Header.vue'
     export default {
         data(){
             return{
                 msg:'hello vue'
             }
+        },
+        components:{
+            appHeader :Header
         }
     }
 </script>
